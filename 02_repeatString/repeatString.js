1520178ed79e99df832a1df74c233ca7722e200a
imthe27 <fertrevi02@gmail.com>
2022-09-02 17:02:35 -0500

    completed 2nd excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let result = '';
+    if (num < 0 || string == null) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num;i++) {
+        result += string;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
