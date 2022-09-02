32938f05f84a5555cf48ce0a3a37499c87c2e23a
imthe27 <fertrevi02@gmail.com>
2022-09-02 17:02:55 -0500

    completed 3rd excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let result = "";
+    for (let i = 1; i <= string.length; i++) {
+        result += string[string.length-i];
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
